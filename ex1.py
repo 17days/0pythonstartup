9490c67c41649f73a360c6863e1ac089a9c022e3
noylau <hualong49@gmail.com>
2016-12-25 12:50:09 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hello Again"
+print "I like Tyoing this."
+print "This is fun."
+print "Yay! Printing."
+print "T'd much rather you 'not'."
+print 'I "said" do not touch this.'
